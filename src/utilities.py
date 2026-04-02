84e50a24598668f307efd893d168d6c44857320c
dansharuka <darukahans@gmail.com>
2026-04-02 10:51:37 +0000

    utilities.py updated

--- src/utilities.py
+++ src/utilities.py
@@ -9,7 +9,7 @@ def filter_outliers(feedbacks):
         if abs(x - mean) <= 1.5:
             filtered.append(x)
         elif abs(x-mean) <= 2.5:
-            filtered.append(x*0.5)
+            filtered.append(mean)
         else:
             pass
 
